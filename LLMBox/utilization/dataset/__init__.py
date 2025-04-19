4d4411b97c6721cf109492bed849bbcd5a63383e
dasoumao <msx@live.ca>
2025-04-19 16:20:16 +0800

    add chatchat, LLMbox and docs

---
+++ LLMBox/utilization/dataset/__init__.py
@@ -0,0 +1,3 @@
+from .dataset import Dataset, DatasetCollection
+from .generation_dataset import GenerationDataset
+from .multiple_choice_dataset import MultipleChoiceDataset
---
+++ LLMBox/utilization/dataset/dataset_utils/__init__.py
@@ -0,0 +1,3 @@
+from .icl_strategies import ICLUtilMixin
+from .raw_dataset_loader import get_raw_dataset_loader
+from .util_mixin import TokenizerUtilMixin
