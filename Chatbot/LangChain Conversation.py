d2f8349f97826f5a4f292cd2fb140f184655530a
Benedict <104419622+Benedict-Y@users.noreply.github.com>
2025-04-20 22:20:31 +0800

    Update LangChain Conversation.py
    Mask the api keys

--- Chatbot/LangChain Conversation.py
+++ Chatbot/LangChain Conversation.py
@@ -6,8 +6,8 @@ def run_langchain_conversation():
     chat_llm = ChatOpenAI(
         model_name="gpt-3.5-turbo",
         openai_api_base="https://api.chatanywhere.tech/v1",
-        openai_api_key="sk-bVWnGBsURxbZSojM6pU3Xmogfbiq6q835INsr1OLrHtBWmgy"
-        # 免费API限制使用deepseek-r1，deepseek-v3，gpt-3.5-turbo，gpt-4o-mini，gpt-4o和embeddings模型
+        openai_api_key="YOUR API KEY"
+        #  Free for deepseek-r1，deepseek-v3，gpt-3.5-turbo，gpt-4o-mini，gpt-4o
     )
     chain = ConversationChain(llm=chat_llm, memory=ConversationBufferMemory())
     while True:
